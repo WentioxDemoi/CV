5c87b6d4a84917ee62b85bb300d59f34ea0df1ca
Rémi Desbordes <remi.desbordes@epitech.eu>
2024-04-04 18:17:25 +0200

    [ADD] Begin content

--- src/App.js
+++ src/App.js
@@ -1,42 +1,21 @@
-import Loading from './Menu/LoadingScreen'
+import Loading from './component/LoadingScreen'
 import { BrowserRouter } from 'react-router-dom';
 import { motion } from 'framer-motion';
 //import { Routes } from 'react-router-dom';
 import React, { useState, useEffect } from 'react';
-import Logo from './Menu/Logo'
+import Logo from './Pages/Logo'
 import Presentation from './Pages/Home'
+import Projet from './Pages/Eprojets'
+
 
 const App = () => {
-  // const [isLoaded, setIsLoaded] = useState(false);
-
-  // // Utilisez useEffect pour simuler le chargement initial du site
-  // useEffect(() => {
-  //   // Simulez un délai de chargement
-  //   const timer = setTimeout(() => {
-  //     setIsLoaded(true);
-  //   }, 3000); // temps en millisecondes, ajustez selon vos besoins
-
-  //   // Assurez-vous de nettoyer le timer lors du démontage du composant
-  //   return () => clearTimeout(timer);
-  // }, []);
 
   return (
     <div className="App">
-      {/* {isLoaded ? ( // Affiche le contenu après le chargement complet */}
-        {/* <motion.div
-          initial={{ opacity: 1 }}
-          animate={{ opacity: 1 }}
-          transition={{ duration: 1 }}
-        >
-          <BrowserRouter>
-          <Loading />  
-          </BrowserRouter>
-        </motion.div> */}
-      {/* ) : ( // Affiche l'écran de chargement pendant le chargement initial */}
       <Loading />  
       <Logo/>
       <Presentation/>
-
+      <Projet/>
     </div>
   );
 };
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,114 +1,90 @@
 import React from 'react';
 import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
 import { motion } from 'framer-motion';
+import github from '../Assets/Github.png'
 
 export default function Home() {
   return (
     <div>
-      {/* <NavBar /> */}
       <div className="relative isolate overflow-hidden bg-white px-6 py-10 sm:py-18 lg:overflow-visible lg:px-0">
-        {/* <div className="absolute inset-0 -z-10 overflow-hidden">
-          <svg
-            className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
-            aria-hidden="true"
-          >
-            <defs>
-              <pattern
-                id="e813992c-7d03-4cc4-a2bd-151760b470a0"
-                width={200}
-                height={200}
-                x="50%"
-                y={-1}
-                patternUnits="userSpaceOnUse"
-              >
-                <path d="M100 200V.5M.5 .5H200" fill="none" />
-              </pattern>
-            </defs>
-            <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
-              <path
-                d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
-                strokeWidth={0}
-              />
-            </svg>
-            <rect width="100%" height="100%" strokeWidth={0} fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
-          </svg>
-        </div> */}
-        {/* <motion.div
-          initial={{ opacity: 0, scale: 0.5 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 1.5 }}
-        > */}
         <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
               <div className="lg:max-w-lg">
-                <p className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</p>
-                <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</h1>
+                <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">Hello World !</h2>
                 <p className="mt-6 text-xl leading-8 text-gray-700">
-                  Aliquet nec orci mattis amet quisque ullamcorper neque, nibh sem. At arcu, sit dui mi, nibh dui, diam
-                  eget aliquam. Quisque id at vitae feugiat egestas.
+                  Je m'appel Rémi Desbordes
                 </p>
               </div>
             </div>
           </div>
-          <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
+          <div className="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden lg:pl-[40%]">
             <img
               className="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem]"
-              src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+              src={github}
               alt=""
             />
           </div>
           <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
             <div className="lg:pr-4">
               <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-                <p>
-                  Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet
-                  vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque
-                  erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris
-                  semper sed amet vitae sed turpis id.
+                
+                <p className="mt-6 text-xl leading-8 text-gray-700">
+                  Si vous êtes là, c'est que vous avez cliqué sur le lien présent sur mon CV et que vous voulez en savoir plus concernant mon profil.
                 </p>
-                <ul role="list" className="mt-8 space-y-8 text-gray-600">
-                  <li className="flex gap-x-3">
-                    <CloudArrowUpIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                    <span>
-                      <strong className="font-semibold text-gray-900">Push to deploy.</strong> Lorem ipsum, dolor sit amet
-                      consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate
-                      blanditiis ratione.
-                    </span>
-                  </li>
-                  <li className="flex gap-x-3">
-                    <LockClosedIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                    <span>
-                      <strong className="font-semibold text-gray-900">SSL certificates.</strong> Anim aute id magna aliqua
-                      ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
-                    </span>
-                  </li>
-                  <li className="flex gap-x-3">
-                    <ServerIcon className="mt-1 h-5 w-5 flex-none text-indigo-600" aria-hidden="true" />
-                    <span>
-                      <strong className="font-semibold text-gray-900">Database backups.</strong> Ac tincidunt sapien
-                      vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.
-                    </span>
-                  </li>
-                </ul>
-                <p className="mt-8">
-                  Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor
-                  fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac
-                  adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.
+
+                <p className="mt-6 text-xl leading-8 text-gray-700">
+                  J'ai 22 ans et je suis tombé dans le chaudron de l'informatique lorsque j'étais petit
                 </p>
-                <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
-                <p className="mt-6">
-                  Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
-                  Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
-                  tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
-                  turpis ipsum eu a sed convallis diam.
+                <p className="mt-6 text-xl leading-8 text-gray-700">
+                  Je fais parti de l'une des dernières générations à avoir connu les téléphones à touches et à avoir appris à coder sans IA générative de texte
                 </p>
+                <p className="mt-6 text-xl leading-8 text-gray-700">
+                  Actuellement à la recherche d'un stage de pré-embauche, je recherche une entreprise
+                </p>
+                <h2 className="text-xl font-bold mt-8 mb-4 text-center">Languages</h2>
+                <div className="flex flex-wrap justify-between mt-10">
+                  <div className="flex flex-col items-center w-1/2 sm:w-1/2 lg:w-1/3 mb-4 lg:mb-0">
+                    <img className="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png" alt="GitHub Logo" />
+                    <p className="text-center mt-4">C</p>
+                  </div>
+                  <div className="flex flex-col items-center w-1/2 sm:w-1/2 lg:w-1/3 mb-4 lg:mb-0">
+                    <img className="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png" alt="GitHub Logo" />
+                    <p className="text-center mt-4">C++</p>
+                  </div>
+                  <div className="flex flex-col items-center w-1/2 sm:w-1/2 lg:w-1/3 mb-4 lg:mb-0">
+                    <img className="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png" alt="GitHub Logo" />
+                    <p className="text-center mt-4">C#</p>
+                  </div>
+                  <div className="flex flex-col items-center w-1/2 sm:w-1/2 lg:w-1/3 mb-4 lg:mb-0">
+                    <img className="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png" alt="GitHub Logo" />
+                    <p className="text-center mt-4">Python</p>
+                  </div>
+                  <div className="flex flex-col items-center w-1/2 sm:w-1/2 lg:w-1/3 mb-4 lg:mb-0">
+                    <img className="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png" alt="GitHub Logo" />
+                    <p className="text-center mt-4">Go</p>
+                  </div>
+                  <div className="flex flex-col items-center w-1/2 sm:w-1/2 lg:w-1/3 mb-4 lg:mb-0">
+                    <img className="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png" alt="GitHub Logo" />
+                    <p className="text-center mt-4">Swift</p>
+                  </div>
+                </div>
+                <h2 className="text-xl font-bold mt-8 mb-4 text-center">Framework</h2>
+                <div className="flex flex-wrap justify-between mt-10">
+                  <div className="flex flex-col items-center w-1/2 sm:w-1/2 lg:w-1/2 mb-4 lg:mb-0">
+                    <img className="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png" alt="GitHub Logo" />
+                    <p className="text-center mt-4">ReactJS</p>
+                  </div>
+                  <div className="flex flex-col items-center w-1/2 sm:w-1/2 lg:w-1/2 mb-4 lg:mb-0">
+                    <img className="h-12 w-12" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/GitHub_Invertocat_Logo.svg/800px-GitHub_Invertocat_Logo.svg.png" alt="GitHub Logo" />
+                    <p className="text-center mt-4">Tailwind CSS</p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        {/* </motion.div> */}
       </div>
     </div>
-  )
+  ); 
 }
