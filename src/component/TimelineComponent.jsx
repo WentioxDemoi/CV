e7bb15554fd3b362cd420186f7629a5dc2d9179b
Rémi Desbordes <remi.desbordes@epitech.eu>
2024-04-10 18:33:45 +0200

    [ADD] Muséomix Hackathon

--- src/component/TimelineComponent.jsx
+++ src/component/TimelineComponent.jsx
@@ -1,4 +1,5 @@
 import { CheckIcon } from '@heroicons/react/20/solid'
+import QRcode from '../Assets/Qrcode.png'
 
 const includedFeaturesEpitech = [
     'Compétences techniques solides',
@@ -8,7 +9,7 @@ const includedFeaturesEpitech = [
 ]
 
 export default function projets() {
-    const handleClick = (id) => {
+    const handleClick = () => {
         const targetElement = document.querySelector('#Circle');
         if (targetElement) {
             window.scrollTo({
@@ -54,7 +55,7 @@ export default function projets() {
                                         En savoir +
                                     </a>
                                     <button
-                                        onClick={() => handleClick("Circle")}
+                                        onClick={handleClick}
                                         className="mt-5 block w-full rounded-md bg-blue-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                     >
                                         Timeline
@@ -68,7 +69,13 @@ export default function projets() {
                         <div className="p-8 sm:p-10 lg:flex-auto">
                             <h3 className="text-2xl font-bold tracking-tight text-gray-900">Les Agences de Papa / Versity</h3>
                             <p className="mt-6 text-base leading-7 text-gray-600">
-                            En tant que stagiaire dans l'équipe IT tout juste créée, j'ai eu l'occasion de participer au développement de ce service. J'ai contribué au changement du VLAN interne de l'entreprise, à l'installation de pare-feu et à l'implémentation de la norme IEEE 802.11ac. J'ai fourni un inventaire matériel et logiciel, des procédures et documentations, des workflows pour le CRM de l'entreprise et des scripts visant à faciliter le travail du service.
+                            En tant que stagiaire dans l'équipe IT tout juste créée, j'ai eu l'occasion de participer au développement de ce service.
+                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            J'ai contribué au changement du VLAN interne de l'entreprise, à l'installation de pare-feu et à l'implémentation de la norme IEEE 802.11ac.
+                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            J'ai fourni un inventaire matériel et logiciel, des procédures et documentations, des workflows pour le CRM de l'entreprise et des scripts visant à faciliter le travail du service.
                             </p>
                             <p className='mt-6 text-base leading-7 text-gray-600'>
                                 Tuteur: Bhupinder Kunwar
@@ -89,7 +96,7 @@ export default function projets() {
                                         En savoir +
                                     </a>
                                     <button
-                                        onClick={() => handleClick("Circle")}
+                                        onClick={handleClick}
                                         className="mt-5 block w-full rounded-md bg-blue-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                     >
                                         Timeline
@@ -132,7 +139,7 @@ export default function projets() {
                                         En savoir +
                                     </a>
                                     <button
-                                        onClick={() => handleClick("Circle")}
+                                        onClick={handleClick}
                                         className="mt-5 block w-full rounded-md bg-blue-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                                     >
                                         Timeline
@@ -141,7 +148,144 @@ export default function projets() {
                             </div>
                         </div>
                     </div>
-                    <h2 className="text-3xl font-bold mt-40 mb-4 text-center">Les hackathons</h2>
+                    <h2 className="text-3xl font-bold mt-40 mb-4 text-center">Hackathons</h2>
+                    <div id="Hackathonactin" className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+                        <div className="p-8 sm:p-10 lg:flex-auto">
+                            <h3 className="text-2xl font-bold tracking-tight text-gray-900">ActinSpace</h3>
+                            <p className="mt-6 text-base leading-7 text-gray-600">
+                            Dans ce hackathon, nous avons eu l'occasion de créer un projet jugé "original" par le jury : Space Care Taker.
+                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            Space Care Taker est une station de service spatiale capable d'effectuer des pleins de carburant, d'imprimer des pièces 3D de satellites grâce aux déchets spatiaux et de réparer des satellites.
+                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            Cette station spatiale est épaulée par de petites navettes capables de dépolluer l'espace, en ramassant les déchets pour l'imprimante 3D, d'effectuer des pleins de carburant et de réparer les satellites. 
+                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            À la suite de cette victoire, nous avons eu l'occasion de visiter les salles blanches de Thales Alenia Space avec Jean Horanieh (<a className="text-blue-500" href="https://www.linkedin.com/in/jean-horanieh-567783216/" target="_blank" rel="noreferrer">Linkedin</a>) et de parler de notre projet avec Yves Durand (<a className="text-blue-500" href="https://www.linkedin.com/in/yves-durand-5a00b075/" target="_blank" rel="noreferrer">Linkedin</a>)
+                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            Nous avons aussi eu l'occasion d'être encadrés sur Space Care Taker par Candace Johnson (<a className="text-blue-500" href="https://www.linkedin.com/in/candace-johnson-2966054a/?originalSubdomain=fr" target="_blank" rel="noreferrer">Linkedin</a>) et Marilyn Davison (<a className="text-blue-500" href="https://www.linkedin.com/in/marilyndavison/" target="_blank" rel="noreferrer">Linkedin</a>).
+                            </p>
+                            <div className="mt-10 flex items-center gap-x-4">
+                                <h4 className="flex-none text-sm font-semibold leading-6 text-gray-600">Prix remporté</h4>
+                                <div className="h-px flex-auto bg-gray-100" />
+                            </div>
+                            <ul className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
+                                    <li className="flex gap-x-3">
+                                        <CheckIcon className="h-6 w-5 flex-none text-blue-700" aria-hidden="true" />
+                                        Première place
+                                    </li>
+                            </ul>
+                        </div>
+                        <div className="p-2 lg:w-56 lg:max-w-md lg:flex-shrink-0 flex-none">
+                            <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:item-center lg:justify-center lg:py-16 h-full flex items-center">
+                                <div className="mx-auto max-w-xs px-8">
+                                    <a
+                                        href="https://www.petitesaffiches.fr/actualites,069/tech,080/spacecaretaker-laureat-de-la-5e,26422.html"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="block w-full rounded-md bg-blue-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        En savoir +
+                                    </a>
+                                    <button
+                                        onClick={handleClick}
+                                        className="mt-5 block w-full rounded-md bg-blue-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Timeline
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="Hackathonmuseo" className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+                        <div className="p-8 sm:p-10 lg:flex-auto">
+                            <h3 className="text-2xl font-bold tracking-tight text-gray-900">Muséomix</h3>
+                            <p className="mt-6 text-base leading-7 text-gray-600">
+                            Dans ce hackathon sur le thème de l'accessibilité dans les musées, nous avons eu l’occasion de nous attaquer à un gros enjeu pour les musées : les enfants.                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            Durant les trois jours de ce hackathon, nous avons relevé plusieurs problèmes. Les enfants ne sont pas forcément assez grands pour lire les cartels, et les cartels ne sont absolument pas ludiques pour eux.                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            Notre objectif a donc été de créer une solution simple à mettre en place pour les musées et ludique pour les enfants.                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            Chi-plouf est alors né. Un simple QR code à scanner pour les parents, une histoire à suivre pour les enfants et la possibilité d’accéder à ces histoires de n’importe où et n’importe quand.                            </p>
+                            <div className="mt-10 flex items-center gap-x-4">
+                                <h4 className="flex-none text-sm font-semibold leading-6 text-gray-600">Prix remporté</h4>
+                                <div className="h-px flex-auto bg-gray-100" />
+                            </div>
+                            <ul className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
+                                    <li className="flex gap-x-3">
+                                        <CheckIcon className="h-6 w-5 flex-none text-blue-700" aria-hidden="true" />
+                                        Première place, coup de coeur Nice Matin
+                                    </li>
+                            </ul>
+                        </div>
+                        <div className="p-2 lg:w-56 lg:max-w-md lg:flex-shrink-0 flex-none">
+                            <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:item-center lg:justify-center lg:py-16 h-full flex items-center">
+                                <div className="mx-auto max-w-xs px-8">
+                                    <a
+                                        href="https://www.nicematin.com/culture/chiplouf-le-nouveau-petit-guide-virtuel-qui-fait-decouvrir-les-secrets-du-musee-des-arts-asiatiques-de-nice-aux-enfants-807484"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="block w-full rounded-md bg-blue-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        En savoir +
+                                    </a>
+                                    <img src={QRcode} className="mx-auto mt-5" alt=""></img>
+                                    <button
+                                        onClick={handleClick}
+                                        className="mt-5 block w-full rounded-md bg-blue-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Timeline
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div id="Hackathonhack" className="mx-auto mt-16 max-w-2xl rounded-3xl ring-1 ring-gray-200 sm:mt-20 lg:mx-0 lg:flex lg:max-w-none">
+                        <div className="p-8 sm:p-10 lg:flex-auto">
+                            <h3 className="text-2xl font-bold tracking-tight text-gray-900">Hack for inclusion</h3>
+                            <p className="mt-6 text-base leading-7 text-gray-600">
+                            Dans ce hackathon sur le thème « Comment rendre les musées plus accessibles ? », nous avons eu l’occasion de nous attaquer à un gros enjeu pour les musées : les enfants.                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            Durant les trois jours de ce hackathon, nous avons relevé plusieurs problèmes. Les enfants ne sont pas forcément assez grands pour lire les cartels, et les cartels ne sont absolument pas ludiques pour eux.                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            Notre objectif a donc été de créer une solution simple à mettre en place pour les musées et ludique pour les enfants.                            </p>
+                            <p className="text-base leading-7 text-gray-600">
+                            Chi-plouf est alors né. Un simple QR code à scanner pour les parents, une histoire à suivre pour les enfants et la possibilité d’accéder à ces histoires de n’importe où et n’importe quand.                            </p>
+                            <div className="mt-10 flex items-center gap-x-4">
+                                <h4 className="flex-none text-sm font-semibold leading-6 text-gray-600">Prix remporté</h4>
+                                <div className="h-px flex-auto bg-gray-100" />
+                            </div>
+                            <ul className="mt-8 grid grid-cols-1 gap-4 text-sm leading-6 text-gray-600 sm:grid-cols-2 sm:gap-6">
+                                    <li className="flex gap-x-3">
+                                        <CheckIcon className="h-6 w-5 flex-none text-blue-700" aria-hidden="true" />
+                                        Première place, coup de coeur Nice Matin
+                                    </li>
+                            </ul>
+                        </div>
+                        <div className="p-2 lg:w-56 lg:max-w-md lg:flex-shrink-0 flex-none">
+                            <div className="rounded-2xl bg-gray-50 py-10 text-center ring-1 ring-inset ring-gray-900/5 lg:flex lg:flex-col lg:item-center lg:justify-center lg:py-16 h-full flex items-center">
+                                <div className="mx-auto max-w-xs px-8">
+                                    <a
+                                        href="https://www.nicematin.com/culture/chiplouf-le-nouveau-petit-guide-virtuel-qui-fait-decouvrir-les-secrets-du-musee-des-arts-asiatiques-de-nice-aux-enfants-807484"
+                                        target="_blank"
+                                        rel="noreferrer"
+                                        className="block w-full rounded-md bg-blue-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        En savoir +
+                                    </a>
+                                    <button
+                                        onClick={handleClick}
+                                        className="mt-5 block w-full rounded-md bg-blue-700 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                    >
+                                        Timeline
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
